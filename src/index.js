77dace162a6f9a90fabdb07d35bd822298348228
hongquang1412 <hongquangst456@gmail.com>
2022-11-08 08:23:02 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import MessengerCustomerChat from "react-messenger-customer-chat";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MessengerCustomerChat pageId="100087297233024" appId="539160330876870" />
       <App />
     </BrowserRouter>
   </React.StrictMode>
